c74b694cb8cecba3c998d2b8f550ddb77e91fc3f
kitsuiwebster <raphmartin1999@gmail.com>
2024-08-10 17:39:18 +0200

    fix: install command

--- index.bash
+++ index.bash
@@ -186,6 +186,35 @@ case $COMMAND in
   access)
     print_urls_and_credentials
     ;;
+  install)
+    echo "👉  Install Minikube"
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo install minikube-linux-amd64 /usr/local/bin/minikube
+    rm minikube-linux-amd64
+
+    echo "👉  Install Kubectl"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+    if [ $? -eq 0 ]; then
+        sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+        rm kubectl kubectl.sha256
+    else
+        echo "kubectl checksum verification failed!"
+        exit 1
+    fi
+
+    echo "👉  Install k9s"
+    K9S_VERSION=$(curl -s https://api.github.com/repos/derailed/k9s/releases/latest | grep tag_name | cut -d '"' -f 4)
+    wget https://github.com/derailed/k9s/releases/download/$K9S_VERSION/k9s_Linux_amd64.tar.gz
+    tar -xvzf k9s_Linux_amd64.tar.gz
+    sudo mv k9s /usr/local/bin/
+    rm k9s_Linux_amd64.tar.gz
+
+    echo "👉  Install Helm"
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+    helm version
+    helm repo update
   *)
     echo -e "\n👉 Usage: $0 {create|delete|stop|start|access}"
     exit 1
